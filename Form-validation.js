7cc544e6e10dcc4f380d1bcf656e1837c0948f00
Nidhi Chouksey <89506754+nidhichouksey72@users.noreply.github.com>
2024-09-28 10:32:53 +0530

    Add files via upload

---
+++ Form-validation.js
@@ -0,0 +1,10 @@
+function validate(){
+    const name= document.getElementById("fullName");
+    const contact =document.getElementById("Contact");
+    const email=document.getElementById("email");
+    const password=document.getElementById("password");
+    if(name.value ==""|| contact.value=="" || email.value==""|| password.value==""){
+        alert("fields are mandatory");
+    }
+    else{
+}}
